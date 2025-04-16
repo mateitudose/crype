e83a9b4a4dbaa0f1e93768e9feb78f1dc8039dda
Matei Tudose <matei@mateitudose.dev>
2025-04-17 00:02:31 +0300

    Modify schema.sql to use enum for order status

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,9 +1,11 @@
+CREATE TYPE order_status AS ENUM ('PENDING', 'PROCESSING','COMPLETED', 'FAILED');
+
 CREATE TABLE orders (
     id UUID PRIMARY KEY,
     amount DECIMAL(10, 2) NOT NULL,
     currency VARCHAR(10) NOT NULL,
     payment_address VARCHAR(255) NOT NULL,
-    status VARCHAR(20) NOT NULL DEFAULT 'PENDING',
+    status order_status NOT NULL DEFAULT 'PENDING',
     created_at TIMESTAMP WITH TIME ZONE NOT NULL,
     order_expiration TIMESTAMP WITH TIME ZONE NOT NULL 
 );
